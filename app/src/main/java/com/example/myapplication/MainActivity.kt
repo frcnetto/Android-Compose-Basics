8c15088622b0ab142566e016c4c80166b6f6fdfb
Netto Cavalcante <frcnetto@gmail.com>
2020-02-09 12:45:51 -0300

    Finished

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,100 @@
-package com.example.myapplication
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.Model
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.layout.ExpandedHeight
+import androidx.ui.layout.Spacing
+import androidx.ui.material.*
+import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
+
+@Model
+class CounterState(var count: Int = 0)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
+            MyAppTheme{
+                Surface {
+                    MyScreenContent()
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(children: @Composable() () -> Unit) {
+    MaterialTheme {
+        Surface(color = Color.Yellow) {
+            children()
+        }
+    }
 }
 
-@Preview
+@Composable
+fun MyScreenContent(
+    names: List<String> = listOf("Android", "there"),
+    counterState: CounterState = CounterState()
+) {
+    Column(modifier = ExpandedHeight) {
+        Column(modifier = Flexible(1f)) {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+        Counter(counterState)
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text (
+        text = "Hello $name!",
+        modifier = Spacing(24.dp),
+        style = (+MaterialTheme.typography()).h1
+    )
+}
+
+@Composable
+fun Counter(state: CounterState) {
+    Button(
+        text = "I've been clicked ${state.count} times",
+        onClick = {
+            state.count++
+        },
+        style = ContainedButtonStyle(color = if (state.count > 5) Color.Green else Color.White)
+    )
+}
+
+val green = Color(0xFF1EB980)
+private val themeColors = ColorPalette(
+    primary = green,
+    surface = Color.DarkGray,
+    onSurface = green
+)
+
+@Composable
+fun MyAppTheme(children: @Composable() () -> Unit) {
+    MaterialTheme(colors = themeColors) {
+        children()
+    }
+}
+
+@Preview("MyScreen preview")
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
+    MyAppTheme{
+        Surface {
+            MyScreenContent()
+        }
     }
 }
